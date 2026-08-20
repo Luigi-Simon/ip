7a9f72a9a73177261ab5d76adb67e9c8569cac2b
Luigi-Simon <sclusoriano@gmail.com>
2026-08-20 18:53:39 +0800

    Add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,31 @@
+/**
+ * Represents a task that occurs during a specific time period.
+ */
+public class Event extends Task {
+    private final String from;
+    private final String to;
+
+    /**
+     * Creates an incomplete event with the given description and time period.
+     *
+     * @param description description of the event
+     * @param from time at which the event starts
+     * @param to time at which the event ends
+     */
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    /**
+     * Returns the event type icon, task details, and time period.
+     *
+     * @return display form of this event
+     */
+    @Override
+    public String toString() {
+        return "[E]" + super.toString()
+                + " (from: " + this.from + " to: " + this.to + ")";
+    }
+}
