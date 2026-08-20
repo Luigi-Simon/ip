553627f9cf8286a133a20410e9aeaa91ed931b64
Luigi-Simon <sclusoriano@gmail.com>
2026-08-20 18:35:27 +0800

    Add Todo class

---
+++ src/main/java/Todo.java
@@ -0,0 +1,24 @@
+/**
+ * Represents a task without a specific date or time.
+ */
+public class Todo extends Task {
+
+    /**
+     * Creates an incomplete todo with the given description.
+     *
+     * @param description description of the todo
+     */
+    public Todo(String description) {
+        super(description);
+    }
+
+    /**
+     * Returns the todo type icon followed by its task details.
+     *
+     * @return display form of this todo
+     */
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
